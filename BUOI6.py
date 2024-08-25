351d0fc61a5f25a98c4b179f29e4eb459c859140
DiemMy0712 <maithidiemmy07122005@gmail.com>
2024-08-25 11:27:10 +0700

    Add files via upload

---
+++ BUOI6.py
@@ -0,0 +1,39 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Aug 20 20:12:25 2024
+
+@author: MAI THI DIEM MY
+CÂU 1: TÁCH THÀNH CÁC SUB-STRING
+dia_chi= "Đại học Quốc gia, Khu phố 6, P. Linh Trung, Q. Thủ Đức, Tp. HCM"
+split_1 =(" Đại học Quốc gia")
+split_2 = ("Khu phố 6")
+split_3 = ("P. Linh Trung")
+split_4 = ("Q. Thủ Đức")
+split_5 = ("Tp. HCM")
+len(dia_chi)
+print("", split_1)
+print("", split_2)
+print("", split_3)
+print("", split_4)
+print("", split_5)
+CÂU 2: 
+    dia_chi= "Đại học Quốc gia, Khu phố 6, P. Linh Trung, Q. Thủ Đức, Tp. HCM"
+    var1= "Đại học Quốc gia"
+    var2= "Khu phố 6"
+    var3= "P. Linh Trung"
+    var4= "Q. Thủ Đức"
+    var5= "Tp. HCM"
+    print("", var1[0:16])
+    print("", var2[0:9])
+    print("", var3[0:13])
+    print("", var4[0:10])
+    print("", var5[0:7])
+CÂU 3: VIẾT CÁC LỆNH CHUYỂN CHUỖI
+input = "i am a cat"
+s1= input.title()
+print(input.title())
+print(s1.swapcase())
+print(input.upper())
+print(input.lower())
+"""
+
